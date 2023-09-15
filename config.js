cd83eadb627d234e597627c34365269933c776b9
Noman <nomansarwar663@gmail.com>
2023-09-15 11:05:40 +0500

    upper level config

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+    basename: '/',
+    defaultPath: '/',
+    userDashboardBasename: '/dashboard',
+    userDashboardDefaultPath: '/dashboard',
+    adminDashboardBasename: '/dashboard/admin',
+    adminDashboardDefaultPath: '/dashboard/admin',
+    fontFamily:
+        '"Cabin",-apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+    borderRadius: 12
+};
+
+export default config;
